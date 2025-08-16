15cd4f451d77a50dfd208a569ccae5940029217d
RanaUniverse <RanaUniverse321@gmail.com>
2025-08-16 20:43:55 +0530

    .env file added where i will keep private things

---
+++ my_modules/bot_config_settings.py
@@ -0,0 +1,62 @@
+"""
+In This file i will keep some constants which is like
+Some Settings Values
+
+"""
+
+import datetime
+from enum import Enum
+import os
+import sys
+
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+BOT_TOKEN_GET = os.environ.get("BOT_TOKEN")
+if BOT_TOKEN_GET is None:
+    print(
+        ".no .env file or env file has not any bot token. "
+        "Please make sure the token is there and re run this program."
+    )
+    sys.exit(1)
+    # i did exit so that in below in bot token it will always a str not none
+
+
+BOT_USERNAME_GET = os.environ.get("BOT_USERNAME", None)
+if not BOT_USERNAME_GET:
+    raise ValueError("❌ BOT_USERNAME not found in .env file!")
+
+
+BOT_TOKEN = f"{BOT_TOKEN_GET}"
+BOT_USERNAME = f"{BOT_USERNAME_GET}"
+
+
+GMT_TIMEZONE = datetime.timezone(datetime.timedelta(hours=0, minutes=0))
+IST_TIMEZONE = datetime.timezone(datetime.timedelta(hours=5, minutes=30))
+
+
+
+# This below values is working for me, so before run the script
+# Make sure to check the values and use it properly
+class MessageEffectEmojies(Enum):
+    """
+    This class content will have some content which help
+    to send the user some message with some animations so that it will looks good...
+    Some Values are as:
+    LIKE = 👍
+    DISLIKE = 👎
+    HEART = ❤️
+    FLAME = 🔥
+    TADA = 🎉
+    POO = 💩
+    """
+
+    LIKE = "5107584321108051014"
+    DISLIKE = "5104858069142078462"
+    HEART = "5159385139981059251"
+    FLAME = "5104841245755180586"
+    TADA = "5046509860389126442"
+    POO = "5046589136895476101"
---
+++ rana_universe.py
@@ -0,0 +1,8 @@
+
+# This is for checking of different own things.
+
+from my_modules import bot_config_settings
+
+BOT_TOKEN = bot_config_settings.BOT_TOKEN
+
+print(BOT_TOKEN)
