673cccfeeb6d49debef3caf2215fc97a6a37f31d
RanaUniverse <RanaUniverse321@gmail.com>
2025-08-16 20:49:27 +0530

    removed the warnings by adding the return None

--- main.py
+++ main.py
@@ -30,6 +30,9 @@ logger = logging.getLogger(__name__)
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
+    if user is None or update.message is None:
+        return None
+    
     await update.message.reply_html(
         rf"Hi {user.mention_html()}!",
         reply_markup=ForceReply(selective=True),
@@ -38,11 +41,16 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
+    if update.message is None:
+        return None
+    
     await update.message.reply_text("Help!")
 
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message."""
+    if update.message is None or update.message.text is None:
+        return None
     await update.message.reply_text(update.message.text)
 
 
